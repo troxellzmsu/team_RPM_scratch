b809a4d4a918e97a7b705f3956db4841ea70b7fd
zach <troxellz@msu.edu>
2022-09-23 21:12:07 -0400

    Add untracked file
    Failed several times to add this file

---
+++ Hello_World/Hello_World/Hello_World/RewardViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Hello_World
+{
+    public class RewardViewModel
+    {
+        private int cost;
+        private string name;
+        private string descriptionOfItem;
+        private bool purchased = false;
+        private string imageName = "Starbucks.png";
+
+        public RewardViewModel(int cost, string name, string description, string imageName)
+        {
+            this.cost = cost;
+            this.name = name;
+            this.descriptionOfItem = description;
+            this.purchased = false;
+            this.imageName = imageName;
+
+        }
+        public int getCost()
+        {
+            return this.cost;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public string getRewardDescription()
+        {
+            return this.descriptionOfItem;
+        }
+        public bool isPurchased()
+        {
+            return this.purchased;
+        }
+        public string getImageName()
+        {
+            return imageName;
+        }
+    }
+}
+
